056f0e6263e0442ab6cc6677f0eb25db847fb0eb
JoaoPMonteiro <j.pedro.silva.monteiro@gmail.com>
2021-12-06 15:42:49 +0000

    added script support to generate onnx and blob files for simple baseline model

--- do_stuff.sh
+++ do_stuff.sh
@@ -5,6 +5,9 @@ fi
 if [ ! -f './onnx_files/videopose_poseaug.onnx' ]; then   
 	python run_evaluate_export.py --posenet_name videopose --keypoints gt --evaluate ./poseaug_baseline/poseaug/videopose/gt/poseaug/ckpt_best_dhp_p1.pth.tar --output_onnx_name onnx_files/videopose_poseaug --device_selected cpu
 fi
+if [ ! -f './onnx_files/mlp_poseaug.onnx' ]; then   
+	python run_evaluate_export.py --posenet_name mlp --keypoints gt --evaluate ./poseaug_baseline/poseaug/mlp/gt/poseaug/ckpt_best_dhp_p1.pth.tar  --output_onnx_name onnx_files/mlp_poseaug --device_selected cpu --stages 2
+fi
 if [ ! -f './onnx_files/stgcn_poseaug.onnx' ]; then   
 	python run_evaluate_export.py --posenet_name stgcn --keypoints gt --evaluate ./poseaug_baseline/poseaug/stgcn/gt/poseaug/ckpt_best_dhp_p1.pth.tar --output_onnx_name onnx_files/stgcn_poseaug --device_selected cpu
 fi
@@ -14,12 +17,20 @@ fi
 if [ ! -f './onnx_files/videopose_baseline.onnx' ]; then   
 	python run_evaluate_export.py --posenet_name videopose --keypoints gt --evaluate ./poseaug_baseline/pretrain_baseline/videopose/gt/pretrain/ckpt_best.pth.tar --output_onnx_name onnx_files/videopose_baseline --device_selected cpu
 fi
+if [ ! -f './onnx_files/mlp_baseline.onnx' ]; then   
+	python run_evaluate_export.py --posenet_name mlp --keypoints gt --evaluate ./poseaug_baseline/pretrain_baseline/mlp/gt/pretrain/ckpt_best.pth.tar  --output_onnx_name onnx_files/mlp_baseline --device_selected cpu --stages 2
+fi
 if [ ! -f './onnx_files/stgcn_baseline.onnx' ]; then   
 	python run_evaluate_export.py --posenet_name stgcn --keypoints gt --evaluate ./poseaug_baseline/pretrain_baseline/stgcn/gt/pretrain/ckpt_best.pth.tar --output_onnx_name onnx_files/stgcn_baseline --device_selected cpu
 fi
 if [ ! -f './onnx_files/gcn_baseline.onnx' ]; then   
 	python run_evaluate_export.py --posenet_name gcn --keypoints gt --evaluate ./poseaug_baseline/pretrain_baseline/gcn/gt/pretrain/ckpt_best.pth.tar --output_onnx_name onnx_files/gcn_baseline --device_selected cpu
 fi
+
+
+
+
+
 
 if [ ! -d blob_files ]; then   
     mkdir blob_files
@@ -28,9 +39,11 @@ fi
 cd blob_files
 
 python ../blobconfiging.py -i ../onnx_files/videopose_poseaug.onnx -n 6 --cselector "wally"
+python ../blobconfiging.py -i ../onnx_files/mlp_poseaug.onnx -n 6 --cselector "wally"
 python ../blobconfiging.py -i ../onnx_files/stgcn_poseaug.onnx -n 6 --cselector "wally"
 python ../blobconfiging.py -i ../onnx_files/gcn_poseaug.onnx -n 6 --cselector "wally"
 
 python ../blobconfiging.py -i ../onnx_files/videopose_baseline.onnx -n 6 --cselector "wally"
+python ../blobconfiging.py -i ../onnx_files/mlp_baseline.onnx -n 6 --cselector "wally"
 python ../blobconfiging.py -i ../onnx_files/stgcn_baseline.onnx -n 6 --cselector "wally"
 python ../blobconfiging.py -i ../onnx_files/gcn_baseline.onnx -n 6 --cselector "wally"
--- do_stuff_mlp.sh
+++ do_stuff_mlp.sh
@@ -2,7 +2,7 @@ if [ ! -d 'onnx_files' ]; then
     mkdir 'onnx_files'
 fi
 
-python run_evaluate_export.py --posenet_name mlp --keypoints gt --evaluate ./poseaug_baseline/poseaug/mlp/gt/poseaug/ckpt_best_dhp_p1.pth.tar  --output_onnx_name onnx_files/mlp_poseaug --device_selected cpu
+python run_evaluate_export.py --posenet_name mlp --keypoints gt --evaluate ./poseaug_baseline/poseaug/mlp/gt/poseaug/ckpt_best_dhp_p1.pth.tar  --output_onnx_name onnx_files/mlp_poseaug --device_selected cpu --stages 2
 
 if [ ! -d blob_files ]; then   
     mkdir blob_files
@@ -10,4 +10,4 @@ fi
 
 
 cd blob_files
-python ../blobconfiging.py -i ../onnx_files/gcn_poseaug.onnx -n 6 --cselector "wally"
+python ../blobconfiging.py -i ../onnx_files/mlp_poseaug.onnx -n 6 --cselector "wally"
