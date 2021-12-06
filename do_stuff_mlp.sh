d5b378c591a3fe00c4c0089cbd5740b2680d06e3
JoaoPMonteiro <j.pedro.silva.monteiro@gmail.com>
2021-12-06 11:31:05 +0000

    scripts folder creation tasks updated

--- do_stuff_mlp.sh
+++ do_stuff_mlp.sh
@@ -3,5 +3,11 @@ if [ ! -d 'onnx_files' ]; then
 fi
 
 python run_evaluate_export.py --posenet_name mlp --keypoints gt --evaluate ./poseaug_baseline/poseaug/mlp/gt/poseaug/ckpt_best_dhp_p1.pth.tar  --output_onnx_name onnx_files/mlp_poseaug --device_selected cpu
-#cd blob_files
-#python ../blobconfiging.py -i ../onnx_files/gcn_poseaug.onnx -n 6 --cselector "wally"
+
+if [ ! -d blob_files ]; then   
+    mkdir blob_files
+fi
+
+
+cd blob_files
+python ../blobconfiging.py -i ../onnx_files/gcn_poseaug.onnx -n 6 --cselector "wally"
--- do_stuff_stgcn.sh
+++ do_stuff_stgcn.sh
@@ -3,5 +3,11 @@ if [ ! -d 'onnx_files' ]; then
 fi
 
 python run_evaluate_export.py --posenet_name stgcn --keypoints gt --evaluate ./poseaug_baseline/poseaug/stgcn/gt/poseaug/ckpt_best_dhp_p1.pth.tar --output_onnx_name onnx_files/stgcn_poseaug --device_selected cpu
+
+if [ ! -d blob_files ]; then   
+    mkdir blob_files
+fi
+
+
 cd blob_files
 python ../blobconfiging.py -i ../onnx_files/stgcn_poseaug.onnx -n 6 --cselector "wally"
